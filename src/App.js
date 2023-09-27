a641b638f8626585313d40484f4e6be34b74f34f
Anna Sadovskaya <anna.sadovskaya.v@gmail.com>
2023-09-27 12:17:52 -0700

    Router Setup

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 // import ItemsList from "./features/items/ItemsList";
 import ItemsMenuPage from "./pages/ItemsMenuPage";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <ItemsMenuPage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="menu" element={<ItemsMenuPage />} />
+      </Routes>
       <Footer />
     </div>
   );
